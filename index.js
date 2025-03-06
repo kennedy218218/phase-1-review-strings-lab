ca9dc730de11e57426296885194a0c2a666dd493
kennedy <kennedy.mwai@student.moringaschool.com>
2025-03-06 21:52:43 +0300

    completed quiz

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser ='kennedy mwai'
+const welcomeMessage = 'Welcome to Flatbook, kennedy mwai {currentUser}!';
+const excitedWelcomeMessage= 'WELCOME TO FLATBOOK, kennedy mwai {KENNEDY MWAI}!';
+const shortGreeting = 'Welcome, k {KENNEDY MWAI}!';
+
